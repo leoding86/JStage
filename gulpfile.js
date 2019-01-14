275c998583fd6fde35b8642c21ddc8bece8a3685
leoding86 <leoding86@gmail.com>
2019-01-14 16:45:02 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
 const {src, dest, parallel, watch} = require('gulp');
-const rollup = require('gulp-rollup');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
+const { rollup } = require('rollup');
+const { uglify } = require('rollup-plugin-uglify');
 
 function js() {
-    return src('./src/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(rollup({
-            input: './src/main.js',
-            output: {
-                format: 'umd',
-                name: 'JStage'
-            }
-        }))
-        // .pipe(uglify())
-        .pipe(rename('JStage.min.js'))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(dest('./dist'));
+    return rollup({
+        input: './src/main.js',
+        plugins: [
+            uglify()
+        ]
+    }).then(function(bundle) {
+        return bundle.write({
+            file: './dist/JStage.min.js',
+            format: 'umd',
+            name: 'JStage',
+            sourcemap: true,
+            compact: true
+        })
+    });
 }
 
 function watchJs() {
@@ -26,6 +25,5 @@ function watchJs() {
 
 exports.js = js;
 exports.watchJs = watchJs;
-exports.default = parallel(js);
-// exports.default = parallel(js, watchJs);
+exports.default = parallel(js, watchJs);
 
